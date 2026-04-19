63e584cbe82f4927075b5bb82ffe5434f3f9ba7b
Krishn Kant Kumar <krishnkantkumar8@gmail.com>
2026-04-19 14:44:09 +0530

    Changed text size

--- frontend/src/components/LoadingScreen.jsx
+++ frontend/src/components/LoadingScreen.jsx
@@ -1,7 +1,7 @@
 function LoadingScreen(){
     return(
         <div className="w-full h-screen fixed flex justify-center items-center top-0 left-0 z-10 backdrop-blur-[2px] text-white">
-            <p className="text-2xl uppercase">Loading ...</p>
+            <p className="text-xl uppercase">Loading ...</p>
         </div>  
     )
 }
