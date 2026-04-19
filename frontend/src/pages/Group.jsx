61550280152fe452c98c1469bb0302f21cac4d1d
Krishn Kant Kumar <krishnkantkumar8@gmail.com>
2026-04-19 14:36:34 +0530

    Added the floating new payment option

--- frontend/src/pages/Group.jsx
+++ frontend/src/pages/Group.jsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom"
 import { ApiContext } from "../contexts/AxiosContext";
 import FloatingPlus from "../components/FloatingPlus";
 import Button from "../components/Button";
+import LoadingScreen from "../components/LoadingScreen";
+import { FileScan, QrCode, UserRound } from "lucide-react";
+import NewExpenseForm from "../components/NewExpenseForm";
 export const Group = () =>{
     const {groupId} = useParams();
     const api = useContext(ApiContext);
@@ -10,10 +13,16 @@ export const Group = () =>{
     const [toggle, setToggle] = useState(false);
     const [openAddMenu, setOpenAddMenu] = useState(false);
     const menuRef = useRef(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [openNewExpenseForm, setOpenNewExpenseForm] = useState(false);
+
     useEffect(()=>{
         api.get(`/users/splits/fetchMembers/${groupId}`)
         .then((res)=>{
             setMembers(res.data.response.members);
+        })
+        .finally(()=>{
+            setIsLoading(false);
         })
     }, [])
 
@@ -23,53 +32,81 @@ export const Group = () =>{
                 setOpenAddMenu(false);
             }
         };
-
+        
         document.addEventListener("mousedown", handleClickOutside);
         
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
         };
     }, [openAddMenu]);
-
+    
     const handleOpenAddMenu = ()=>{
         setOpenAddMenu(true);
     }
 
-    return(
-        <div className="flex flex-col h-[calc(100vh-55px)] w-full p-5 items-center">
-            <div className="flex justify-center items-center">
-                <div className={`px-4 py-2 text-${toggle ? 'gray-400' : 'brand-primary'} bg-charcol-${toggle ? '' : 'primary'} border-1 border-charcol-primary text-sm cursor-pointer`} onClick={()=>{setToggle(false)}}>Overview</div>
-                <div className={`px-4 py-2 text-${toggle ? 'brand-primary' : 'gray-400'} bg-charcol-${toggle ? 'primary' : ''} border-1 border-charcol-primary text-sm cursor-pointer`} onClick={()=>{setToggle(true)}}>Expenses</div>
-            </div>
+    const handleNewExpense = ()=>{
+        setOpenNewExpenseForm(true);
+        setOpenAddMenu(false);
+    }
+    const handleNewPayment = ()=>{
+        console.log('New Payment Added');
+    }
+    const handleNewMember = ()=>{
+        console.log('New Member Added');
+    }
 
-            <div className="h-full w-full max-w-[750px] p-3 overflow-y-auto">
-                <div className="h-[80%] w-full flex flex-col items-center gap-1 overflow-y-auto">
-                    {members.map((member)=>{
-                        return(
-                            <div key={member._id} className="w-[90%] flex justify-between bg-charcol-primary p-2 px-4 rounded">
-                                <p className="text-white">{member.name}</p>
-                                <p className={`text-${member.amount < 0 ? 'orange' : 'brand'}-primary`}>{member.amount < 0 ? '- ' : '+ '}{member.amount}</p>
+    const cut = ()=>{
+        setOpenNewExpenseForm(false);
+    }
+    
+    if(isLoading) return <LoadingScreen/>
+    return(
+        <>
+            {openNewExpenseForm ? <NewExpenseForm cut={cut} members={members}/> : ''}
+            <div className="flex flex-col h-[calc(100vh-55px)] w-full p-5 items-center">
+                <div className="flex justify-center items-center">
+                    <div className={`px-4 py-2 text-${toggle ? 'gray-400' : 'brand-primary'} bg-charcol-${toggle ? '' : 'primary'} border-1 border-charcol-primary text-sm cursor-pointer`} onClick={()=>{setToggle(false)}}>Overview</div>
+                    <div className={`px-4 py-2 text-${toggle ? 'brand-primary' : 'gray-400'} bg-charcol-${toggle ? 'primary' : ''} border-1 border-charcol-primary text-sm cursor-pointer`} onClick={()=>{setToggle(true)}}>Expenses</div>
+                </div>
+
+                <div className="h-full w-full max-w-[750px] p-3 overflow-y-auto">
+                    <div className="h-[80%] w-full flex flex-col items-center gap-1 overflow-y-auto">
+                        {members.map((member)=>{
+                            return(
+                                <div key={member._id} className="w-[90%] flex justify-between bg-charcol-primary p-2 px-4 rounded text-sm">
+                                    <p className="text-white">{member.name}</p>
+                                    <p className={`text-${member.amount < 0 ? 'orange' : 'brand'}-primary`}>{member.amount < 0 ? '- ' : '+ '}{member.amount}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="mt-6">
+                        <div className="flex flex-col justify-center items-center gap-2">
+                            <p className="text-gray-400 bg-charcol-primary w-45 text-center text-sm py-2 rounded hover:text-white cursor-pointer">Suggested Payments</p>
+                            <p className="text-gray-400 bg-charcol-primary w-45 text-center text-sm py-2 rounded hover:text-white cursor-pointer">Download Summary</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${openAddMenu ? 'hidden' : ''}`} onClick={handleOpenAddMenu}><FloatingPlus/></div>
+                <div ref={menuRef}>
+                    {openAddMenu ? (
+                        <div className="flex flex-col absolute bottom-5 right-5 backdrop-blur-[5px] gap-4 p-5">
+                            <div className="text-brand-primary text-xs flex justify-end items-center cursor-pointer" onClick={handleNewExpense}>
+                                <p>New Expense</p>
+                                <div className="p-2 ml-2 bg-brand-primary rounded-full text-black shadow-primary hover:bg-brand-dark"><FileScan size={20}/></div>
                             </div>
-                        )
-                    })}
-                </div>
-                <div className="mt-6">
-                    <div className="flex flex-col justify-center items-center gap-2">
-                        <p className="text-gray-400 bg-charcol-primary w-45 text-center py-2 rounded hover:text-white cursor-pointer">Suggested Payments</p>
-                        <p className="text-gray-400 bg-charcol-primary w-45 text-center py-2 rounded hover:text-white cursor-pointer">Download Summary</p>
-                    </div>
+                            <div className="text-brand-primary text-xs flex justify-end items-center cursor-pointer" onClick={handleNewPayment}>
+                                <p>New Payment</p>
+                                <div className="p-2 ml-2 bg-brand-primary rounded-full text-black shadow-primary hover:bg-brand-dark"><QrCode size={20}/></div>
+                            </div>
+                            <div className="text-brand-primary text-xs flex justify-end items-center cursor-pointer" onClick={handleNewMember}>
+                                <p>New Member</p>
+                                <div className="p-2 ml-2 bg-brand-primary rounded-full text-black shadow-primary hover:bg-brand-dark"><UserRound size={20}/></div>
+                            </div>
+                        </div>
+                    ) : ''}
                 </div>
             </div>
-            <div className="" onClick={handleOpenAddMenu}><FloatingPlus/></div>
-            <div ref={menuRef}>
-                {openAddMenu ? (
-                    <div className="flex flex-col absolute bottom-5 right-5 rounded bg-charcol-primary gap-2 p-4 border-2 border-charcol-light">
-                        <Button width={40} text='New Expense'/>
-                        <Button width={40} text='New Payment'/>
-                        <Button width={40} text='New Member'/>
-                    </div>
-                ) : ''}
-            </div>
-        </div>
+        </>
     )
 }
