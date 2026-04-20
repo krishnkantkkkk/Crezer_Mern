681f531e8f178a282a9cde5627452c044f55c35d
Krishn Kant Kumar <krishnkantkumar8@gmail.com>
2026-04-20 15:55:14 +0530

    corrected the Location

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import {Routes, Route} from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Register from './pages/Register'
 import Login from './pages/Login'
