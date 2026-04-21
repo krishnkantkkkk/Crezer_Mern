4c77c4102f12162f4823639739f08b046da95d52
Krishn Kant Kumar <krishnkantkumar8@gmail.com>
2026-04-21 22:19:24 +0530

    added await in db connection

--- backend/configuration/db.js
+++ backend/configuration/db.js
@@ -2,11 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGODB_URI).then(()=>{
-        console.log("Connected to MongoDB");
-    }).catch((err)=>{
-        console.error("Error connecting to MongoDB:", err);
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
